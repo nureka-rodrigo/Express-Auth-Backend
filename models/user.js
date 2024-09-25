37c95ec6b936c027831e075b22df892be63dc408
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-09-25 21:50:06 +0530

    Add user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+});
+
+export const User = model("User", userSchema);
